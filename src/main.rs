153023eef4d0ddaf0ad74dfbdcb7814b8717e658
Matthew Hill <GeistInDerSH@gmail.com>
2025-12-04 10:04:54 +0100

    Run Each Solved Day
    Ensure that each day that has a solution gets run

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
     println!("Advent of Code 2025");
     advent_of_code::day_1::run();
+    advent_of_code::day_2::run();
+    advent_of_code::day_3::run();
+    advent_of_code::day_4::run();
 }
